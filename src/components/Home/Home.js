bba20867081969b092ba3c20fc4b614dd8617ca6
https://github.com/AdilMahmoudRion <adilmahmoudrion@gmail.com>
2021-10-31 02:47:46 +0600

    added log in from

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 import Hotels from '../Hotels/Hotels';
 
 const Home = () => {
     return (
       <div className="container">
-        <h1>Home</h1>
+       <Banner></Banner>
         <Hotels></Hotels>
       </div>
     );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,74 @@
-import React from 'react';
-import useAuth from '../../hooks/useAuth';
-
+import React from "react";
+import "./Login.css";
+import useAuth from "../../hooks/useAuth";
 const Login = () => {
-    const {signInUsingGoogle}=useAuth();
-    return (
-        <div>
-            <button onClick={signInUsingGoogle}>log in</button>
+  const { signInUsingGoogle } = useAuth();
+  return (
+    <div className="body">
+    
+        <div class="form-container">
+          <form id="contact" action="" method="post">
+            <h3>Login Form</h3>
+            <h4>Login now then booking you site</h4>
+            <fieldset>
+              <input
+                placeholder="Your name"
+                type="text"
+                tabindex="1"
+                required
+                autofocus
+              />
+            </fieldset>
+            <fieldset>
+              <input
+                placeholder="Your Email Address"
+                type="email"
+                tabindex="2"
+                required
+              />
+            </fieldset>
+            <fieldset>
+              <input
+                placeholder="Your Phone Number (optional)"
+                type="tel"
+                tabindex="3"
+                required
+              />
+            </fieldset>
+            <fieldset>
+              <input
+                placeholder="Type Password"
+                type="pass"
+                tabindex="4"
+                required
+              />
+            </fieldset>
+            
+            <fieldset>
+              <button
+                name="submit"
+                type="submit"
+                id="contact-submit"
+                data-submit="...Sending"
+              >
+                Submit
+              </button>
+            </fieldset>
+            <fieldset>
+              <button
+                name="submit"
+                type="submit"
+                className="signInUsingGoogle"
+                onClick={signInUsingGoogle}
+              >
+                Sign With Google
+              </button>
+            </fieldset>
+          </form>
         </div>
-    );
+      </div>
+
+  );
 };
 
 export default Login;
