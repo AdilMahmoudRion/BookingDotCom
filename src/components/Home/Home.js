c089549bcb200a06e114cdc779947729fdb648fe
https://github.com/AdilMahmoudRion <adilmahmoudrion@gmail.com>
2021-10-31 19:19:17 +0600

    service section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Hotels from '../Hotels/Hotels';
+import Login from '../Login/Login';
+import OurService from '../OurService/OurService';
 
 const Home = () => {
     return (
       <div className="">
        <Banner></Banner>
         <Hotels></Hotels>
+        <OurService></OurService>
+        <Login></Login>
       </div>
     );
 };
