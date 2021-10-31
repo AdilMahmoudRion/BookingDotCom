642d5ee4e425f448455d81a013906894e9c1f204
https://github.com/AdilMahmoudRion <adilmahmoudrion@gmail.com>
2021-10-31 14:35:24 +0600

    banner edit

--- src/components/AddHotel/AddHotel.js
+++ src/components/AddHotel/AddHotel.js
@@ -6,20 +6,20 @@ const AddHotel = () => {
   const {
     register,
     handleSubmit,
-      formState: { errors },
-    reset
+    formState: { errors },
+    reset,
   } = useForm();
-    const onSubmit = (data) => {
-        axios
-          .post("https://gentle-mountain-63376.herokuapp.com/addhotel", data)
-          .then((res) => {
-            if (res.data.insertedId) {
-              alert("added successfully");
-              reset();
-            }
-          });
-        
-    };
+  const onSubmit = (data) => {
+    axios
+      .post("https://gentle-mountain-63376.herokuapp.com/addhotel", data)
+      .then((res) => {
+        if (res.data.insertedId) {
+          alert("added successfully");
+          reset();
+        }
+      });
+  
+  };
   return (
     <div>
       <h1>Please Add a Service</h1>
@@ -30,6 +30,25 @@ const AddHotel = () => {
           {...register("HotelName", { required: true })}
         />
         {errors.HotelName && <p>Hotel name is required.</p>}
+        <input
+          placeholder="Hotel Retting"
+          {...register("rating", { min: 1, max: 10 }, { required: true })}
+        />
+        {errors.rating && (
+          <p className="text-muted">Rating required Out of 10.</p>
+        )}
+        <input
+          placeholder="Address"
+          type="text"
+          {...register("address", { min: 1, max: 10 }, { required: true })}
+        />
+        {errors.address && <p>Address required.</p>}
+        <input
+          placeholder="Rate"
+          type="number"
+          {...register("rate", { required: true })}
+        />
+        {errors.rate && <p>Rate required.</p>}
         <input
           placeholder="insert Hotel image link"
           {...register("img", { required: true })}
--- src/components/Hotel/Hotel.js
+++ src/components/Hotel/Hotel.js
@@ -1,39 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Hotel.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Hotel.css";
 const Hotel = (props) => {
-  const { HotelName, Details, img, _id } = props.hotel;
-    return (
-      <div>
-        <div className="hotel-information">
-          <div className="logo">
-            <img className="hotel-img" src={img} alt="" />
-          </div>
-          <div className="hotel-details">
-            <div className="name-ret">
-              <h5>{HotelName}</h5>
+  const { HotelName, Details, img, _id, rate, address, rating } = props.hotel;
+  const shorten = address ? address.substring(0, 70) : "";
+ 
+
+  return (
+    <div>
+      <div className="hotel-information">
+        <div className="logo">
+          <img className="hotel-img" src={img} alt="" />
+        </div>
+        <div className="hotel-details">
+          <div className="name-ret">
+            <h5>{HotelName}</h5>
+            <div>
               <p className="reviews">
-                reviews <span className="span-tag">8.5</span>
+                reviews <span className="span-tag">{rating}</span>
               </p>
             </div>
-            <p>Address</p>
-            <h4>
-              <span className="text-muted">From: </span>
-              <strong>BDT 4,197</strong>
-            </h4>
-            <div className="cnf-button">
-              <button className="booking-button btn1 w-50 me-1">
-                Booking Now
-              </button>
-              {/* <Link className="m-0 p-0" to={`/bookingHotel/${_id}`}> */}
-              <button className="booking-button btn2 w-50">Details More</button>
-              {/* </Link> */}
-              {/* to={`/bookingHotel/${_id}`} */}
-            </div>
+          </div>
+          <p>Address: {shorten}...</p>
+          <h4>
+            <span className="text-muted">From: </span>
+            <strong className="rate">BDT {rate}</strong>
+          </h4>
+          <div className="cnf-button">
+            <Link
+              className="p-1 text-center booking-button btn1 w-50"
+              to={`/bookingHotel/${_id}`}
+            >
+              Details{" "}
+            </Link>
+            <Link
+              className="text-center p-1 booking-button btn2 w-50"
+              to={`/bookingHotel/${_id}`}
+            >
+              Booking Now{" "}
+            </Link>
+ 
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Hotel;
